4220dd3a5fffaf669bcebeeb994704398aae022f
bhawana-web <bhawnav161102@gmail.com>
2025-01-01 21:20:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // complete this js code
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
+}
